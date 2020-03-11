ffe7472bc63bc6cce90ea4ce9944b5144c210c71
Geoffrey Merck <F4FXL@users.noreply.github.com>
2020-03-11 18:53:08 +0100

    Update version and readme

--- SGSXLApp.cpp
+++ SGSXLApp.cpp
@@ -103,7 +103,7 @@ bool CSGSXLApp::createThread()
 		bool isQuadNet;
 		config.getIrcDDB(i, hostname, username, password, isQuadNet);
 		std::cout << "ircDDB " << i + 1 << " set to " << hostname << " username set to " << username << " QuadNet " << isQuadNet << std::endl;
-		CIRCDDB *ircDDB = new CIRCDDBClient(hostname, 9007U, username, password, std::string("linux_SmartGroupServer") + std::string("-") + VERSION, address, isQuadNet);
+		CIRCDDB *ircDDB = new CIRCDDBClient(hostname, 9007U, username, password, std::string("linux_SmartGroupServerXL-") + VERSION, address, isQuadNet);
 		clients.push_back(ircDDB);
 	}
 	
